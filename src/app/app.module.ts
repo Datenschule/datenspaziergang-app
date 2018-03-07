5889c53a3b2602cb0b188774e478327b95252e6c
Knut Perseke <knut.perseke@okfn.de>
2018-03-07 10:01:19 +0100

    add new components to aapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,20 +20,18 @@ import {HttpClientModule} from "@angular/common/http";
 import { CoursesService } from './services/courses/courses.service';
 import {RouterModule} from "@angular/router";
 import { CourseDetailComponent } from './pages/course-detail/course-detail.component';
-import { CourseMapComponent } from './pages/course-map/course-map.component';
 import { MapCellComponent } from './components/map-cell/map-cell.component';
 import { WaypointInfoComponent } from './components/waypoint-info/waypoint-info.component';
 import { WaypointDetailComponent } from './pages/waypoint-detail/waypoint-detail.component';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
-import { OverviewMapComponent } from './components/page-components/overview-map-component/overview-map.component';
-import { PointToPointMapComponent } from './components/page-components/point-to-point-map-component/point-to-point-map.component';
-import { StoryComponent } from './components/page-components/story-component/story.component';
-import { QuizComponent } from './components/page-components/quiz-component/quiz-component';
-import { SuccessComponent } from './components/page-components/success-component/success.component';
-import { ErrorComponent } from './components/page-components/error/error.component';
-import { PageContainerComponent } from './components/page-container/page-container.component';
 import { PageDirective } from './directives/page.directive';
 import { NextPageService } from './services/next-page/next-page.service';
+
+import {OverviewMapComponent} from './pages/course-pages/overview-map/overview-map.component';
+import {PointToPointMapComponent} from './pages/course-pages/point-to-point-map/point-to-point-map.component';
+import {StoryComponent} from './pages/course-pages/story/story.component';
+import {QuizComponent} from './pages/course-pages/quiz/quiz.component';
+import {SuccessComponent} from './pages/course-pages/success/success.component';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
@@ -47,7 +45,6 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     ActionBarComponent,
     CardComponent,
     CourseDetailComponent,
-    CourseMapComponent,
     MapCellComponent,
     WaypointInfoComponent,
     WaypointDetailComponent,
@@ -57,17 +54,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     StoryComponent,
     QuizComponent,
     SuccessComponent,
-    ErrorComponent,
-    PageContainerComponent,
     PageDirective,
-  ],
-  entryComponents: [
-    ErrorComponent,
-    OverviewMapComponent,
-    PointToPointMapComponent,
-    QuizComponent,
-    StoryComponent,
-    SuccessComponent
   ],
   imports: [
     BrowserModule,

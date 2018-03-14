1f7ca861063594ee8487734c4aa07c6accc55d95
Knut Perseke <knut.perseke@okfn.de>
2018-03-14 14:43:09 +0100

    styling for success screen

--- src/app/pages/course-pages/success/success.component.ts
+++ src/app/pages/course-pages/success/success.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {CoursesService} from '../../../services/courses/courses.service';
+import {ActivatedRoute} from '@angular/router';
+import {Stations} from '../../../model/stations';
+import SuccessStation = Stations.SuccessStation;
+import {Course} from '../../../model/course';
 
 @Component({
   selector: 'app-success',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
+  course: Course;
+  station: SuccessStation;
+
+  constructor(private coursesService: CoursesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const course_id = +this.route.snapshot.paramMap.get('course');
+    const station_id = +this.route.snapshot.paramMap.get('id');
+    this.coursesService.getCourse(course_id).subscribe((course) => {
+
+      this.course = course;
+      this.station = <SuccessStation>this.course.stations.find((station) => station.id === station_id);
+    });
   }
 
 }

f2bdfd95f8d6f3e73baa311f234367c4721814d8
Knut Perseke <knut.perseke@okfn.de>
2018-04-12 15:34:48 +0200

    add new intro screen

--- src/app/pages/intro/intro.component.ts
+++ src/app/pages/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
-  constructor() { }
+  title = 'Datenspaziergang';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }

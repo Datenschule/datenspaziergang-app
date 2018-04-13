2f98c7c34dcd8dea0a1c57ccb0b30477751dce34
Knut Perseke <knut.perseke@okfn.de>
2018-04-13 15:01:39 +0200

    some refinement on story component

--- src/app/pages/course-pages/story/story.component.ts
+++ src/app/pages/course-pages/story/story.component.ts
@@ -13,7 +13,8 @@ export class StoryComponent implements OnInit {
 
   nextLink: string;
   image: SafeStyle;
-  story: StoryPage;
+  title: string;
+  story: any;
 
   constructor(private coursesService: CoursesService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
   }
@@ -22,13 +23,16 @@ export class StoryComponent implements OnInit {
     this.route.params.subscribe((params) => {
       const course_id = +params['course'];
       const station_id = +params['station'];
+      const subject_id = +params['subject'];
       const page_id = +params['page'];
 
-      this.coursesService.getPage(course_id, station_id, page_id).subscribe((page) => {
 
+      this.coursesService.getPage(course_id, station_id, subject_id, page_id).subscribe((page) => {
+
+        this.title = page.name;
         this.story = page;
-        this.image = this.sanitizer.bypassSecurityTrustStyle(`url(${this.story.img})`);
-        this.coursesService.getNextPageLink(course_id, station_id, page.next).subscribe((nextPage) => {
+        this.image = this.sanitizer.bypassSecurityTrustStyle(`url(${page.img})`);
+        this.coursesService.getNextPageLink(course_id, station_id, subject_id, page.next).subscribe((nextPage) => {
           this.nextLink = nextPage;
         });
       });

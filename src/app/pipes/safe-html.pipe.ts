af03f8f0b4353cd92c7fef7d78570f0b249cf4f3
Knut Perseke <knut.perseke@okfn.de>
2018-04-13 14:55:36 +0200

    update pipe to sanitize html

--- src/app/pipes/safe-html.pipe.ts
+++ src/app/pipes/safe-html.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Pipe({
   name: 'safeHtml'
@@ -8,7 +8,7 @@ export class SafeHtmlPipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(value: any, args?: any): any {
+  transform(value: string): SafeHtml {
     console.log(value);
     return this.sanitizer.bypassSecurityTrustHtml(value);
   }

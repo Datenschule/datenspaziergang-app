ec5c5e10c1de6d5e98bb333f719c62bdc024bef9
lislis <mail@lislis.de>
2018-04-17 16:52:33 +0200

    poking sass

--- src/app/components/action-bar/action-bar.component.ts
+++ src/app/components/action-bar/action-bar.component.ts
@@ -9,13 +9,13 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   animations: [
     trigger('slideInOut', [
       state('in', style({
-        transform: 'translate3d(100%, 0, 0)'
+        transform: 'translate3d(0, 0, 0)'
       })),
       state('out', style({
-        transform: 'translate3d(0,0,0)'
+        transform: 'translate3d(-100%,0,0)'
       })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
+      transition('in => out', animate('200ms ease-in-out')),
+      transition('out => in', animate('300ms ease-in-out'))
     ])
   ]
 })

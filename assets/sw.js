ccdcfb421c7e1c349268f94bdbd2c6deb4c2de3b
Travis <travis@travis-ci.org>
2018-04-19 11:53:50 +0000

    Travis build 86

---
+++ assets/sw.js
@@ -0,0 +1,12 @@
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});

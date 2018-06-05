0a51a13a242e76c254b23454da03122c66ef19e1
Knut Perseke <knut.perseke@okfn.de>
2018-06-05 15:49:19 +0200

    update text

--- src/app/pages/course-pages/success/success.component.ts
+++ src/app/pages/course-pages/success/success.component.ts
@@ -12,7 +12,7 @@ import {Station} from '../../../model/stations';
 })
 export class SuccessComponent implements OnInit {
 
-  message = "Herzlichen Glückwunsch, besuchen uns auf datenschule.de für unsere Workshops";
+  message = "Vielen Dank, dass du an unseren Datenspaziergang durch Berlin-Kreuzberg teilgenommen hast! Wir hoffen es hat dir gefallen!\n";
   title = "Abschluss";
 
   constructor(private coursesService: CoursesService, private route: ActivatedRoute) {

ffba2453e8f209822194ac5ad68b460f3256bcf7
Knut Perseke <knut.perseke@okfn.de>
2018-07-09 11:30:01 +0200

    change app to user external api
    adjust components
    
    add more files to service worker cache
    
    change texts
    
    more sw assets

--- src/app/pages/course-detail/course-detail.component.ts
+++ src/app/pages/course-detail/course-detail.component.ts
@@ -1,29 +1,71 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../../model/course";
-import {CoursesService} from "../../services/courses/courses.service";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Course} from '../../model/course';
+import {CoursesService} from '../../services/courses/courses.service';
+import {environment} from '../../../environments/environment';
+import * as turf from '@turf/turf';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.scss']
+  styleUrls: ['./course-detail.component.scss'],
+  animations: [
+    trigger('swiping', [
+      state('left', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('right', style({
+        transform: 'translate3d(-99%, 0, 0)'
+      })),
+      transition('left => right', animate('200ms ease-in-out')),
+      transition('right => left', animate('200ms ease-in-out'))
+    ])
+  ]
 })
 export class CourseDetailComponent implements OnInit {
+  title = "Tourdetail";
+  activeCourse: Course;
+  activeLine: any;
 
-  title = "Detail";
-  course: Course;
-  entryLink: string;
+  swipeState = 'left';
 
-  constructor(private route: ActivatedRoute, private coursesService: CoursesService) { }
+  mapOptions = {
+    style: environment.mapboxTiles.street,
+    zoom: [14],
+    center: [13.40497, 52.517221]
+  };
+
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService) {}
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.coursesService.getCourse(id).subscribe((course) => {
-      console.log('fetched course!!!!');
-      console.log(course);
-      this.course = course;
-      this.entryLink = `/overview-map/${this.course.id}`;
-    });
+    this.route.params.subscribe((params) => {
+      const course_id = +params['id'];
+
+      this.coursesService.getCourse(course_id).subscribe((course) => {
+        if (course.status === 'success') {
+          this.activeCourse = course.data.walk;
+          this.title = `Tourdetail ${this.activeCourse.name}`;
+
+          let features = turf.featureCollection(
+            this.activeCourse.stations.map(station => turf.point([station.position.lon, station.position.lat]))
+          );
+
+          let center = turf.center(features);
+          center = turf.transformTranslate(center, -0.8, 90);
+          this.mapOptions.center = center.geometry.coordinates;
+          this.activeLine = this.activeCourse.stations.reduce((prev, curr) => {
+            console.log(prev);
+            prev.push([curr.position.lon, curr.position.lat]);
+            return prev;
+          }, []);
+        }
+      });
+    })
   }
 
+  toggleSwipeState() {
+    this.swipeState = this.swipeState === 'left' ? 'right' : 'left';
+  }
 }
--- src/app/pages/course-pages/success/success.component.ts
+++ src/app/pages/course-pages/success/success.component.ts
@@ -12,13 +12,22 @@ import {Station} from '../../../model/stations';
 })
 export class SuccessComponent implements OnInit {
 
-  message = "Vielen Dank, dass du an unseren Datenspaziergang durch Berlin-Kreuzberg teilgenommen hast! Wir hoffen es hat dir gefallen!\n";
+  message = "";
   title = "Abschluss";
+  course: Course;
 
-  constructor(private coursesService: CoursesService, private route: ActivatedRoute) {
+  constructor(private coursesService: CoursesService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-
+    const course_id = +this.route.snapshot.paramMap.get('course');
+    this.coursesService.getCourse(course_id).subscribe((course) => {
+      if (course.status === 'success') {
+        this.course = course.data.walk;
+        this.title = `Abschluss ${this.course.name}`;
+        this.message = `Vielen Dank, dass du am Datenspaziergang durch ${this.course.name} durch ${this.course.location} teilgenommen hast! Wir hoffen es hat dir gefallen!`
+      }
+    });
   }
 }
